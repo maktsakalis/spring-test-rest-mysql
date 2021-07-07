503b3a92743d7c82b255a0aa7d26939bad2f7343
maktsakalis <maktsakalis@gmail.com>
2021-07-07 20:59:48 +0300

    minor changes

---
+++ src/test/java/com/mycompany/springtest2_rest_mysql/EmployeeRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.mycompany.springtest2_rest_mysql;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class EmployeeRepositoryTest {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeRepositoryTest(EmployeeRepository employeeRepository) {
+        super();
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
